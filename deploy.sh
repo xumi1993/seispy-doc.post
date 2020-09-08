31dd46455a04d37638dacbf0e91be4231ef62e2e
Mijian Xu <gomijianxu@gmail.com>
2020-09-09 01:59:06 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 cd public
 
-if [ -n "$GITHUB_AUTH_SECRET" ]
+if [ -n "$GITHUB_TOKEN" ]
 then
     touch ~/.git-credentials
     chmod 0600 ~/.git-credentials
-    echo $GITHUB_AUTH_SECRET > ~/.git-credentials
+    echo "https://${GITHUB_TOKEN}:@github.com" > ~/.git-credentials
     git config credential.helper store
-    git config user.email "mijian.xu@smail.nju.edu.cn"
+    git config user.email "gomijianxu@gmail.com"
     git config user.name "xumi1993"
 fi
 git add .
